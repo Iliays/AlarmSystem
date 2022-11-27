f3c3d919b38e0b5b161ec7fc288ec7342e178d1f
Iliays <blacking228@gmail.com>
2022-11-27 18:22:47 +0500

    Change

--- Platformer2d/Assets/Scripts/AlarmSystem.cs
+++ Platformer2d/Assets/Scripts/AlarmSystem.cs
@@ -1,27 +1,21 @@
 using UnityEngine;
 using DG.Tweening;
+using System.Collections;
 
 [RequireComponent(typeof(AudioSource))]
 public class AlarmSystem : MonoBehaviour
 {
 	[SerializeField] private AudioSource _alarm;
 
-	private float _maxVolume = 1f;
-	private float _minVolume = 0f;
-	private float _duration = 3f;
+	private Tweener _tween;
 
 	private void Start()
 	{
 		_alarm = GetComponent<AudioSource>();
 	}
 
-	private void OnTriggerEnter2D(Collider2D collision)
+	public void ChangeVolume(float volume, float duration)
 	{
-		DOTweenModuleAudio.DOFade(_alarm, _maxVolume, _duration);
-	}
-
-	private void OnTriggerExit2D(Collider2D collision)
-	{
-		DOTweenModuleAudio.DOFade(_alarm, _minVolume, _duration);
+		_tween = DOTweenModuleAudio.DOFade(_alarm, volume, duration);
 	}
 }
---
+++ Platformer2d/Assets/Scripts/HouseCollisionHandler.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AlarmSystem))]
+public class HouseCollisionHandler : MonoBehaviour
+{
+	[SerializeField] private AlarmSystem _alarmSystem;
+	private float _maxVolume = 1f;
+	private float _minVolume = 0f;
+	private float _duration = 3f;
+
+	private void Start()
+	{
+		_alarmSystem = GetComponent<AlarmSystem>();
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		_alarmSystem.ChangeVolume(_maxVolume, _duration);
+	}
+
+	private void OnTriggerExit2D(Collider2D collision)
+	{
+		_alarmSystem.ChangeVolume(_minVolume, _duration);
+	}
+}
